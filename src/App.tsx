90552be63b647020f9b59d2dc03b0f94f07b8c57
German Bortoli <germanazo@gmail.com>
2024-03-12 12:29:09 -0300

    Add checkbox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,95 @@
 import { useState } from "react";
+// import { allCountries } from "country-region-data";
 import "./App.css";
 
+interface CountryItem {
+  name: string;
+  checked: boolean;
+  countryCode: string;
+}
+
+// Mock all countries array
+const allCountries = [
+  ["India", "IN"],
+  ["USA", "US"],
+  ["France", "FR"],
+];
+
+// Generate a map object to facilitate the access to the country data
+const countryMap = new Map(
+  allCountries.map((country) => [
+    country[1],
+    { name: country[0], checked: false, countryCode: country[1] },
+  ])
+);
+
 function App() {
-  const [count, setCount] = useState(0);
+  // Append the country data to the state
+  const [countries, setCountries] =
+    useState<Map<string, CountryItem>>(countryMap);
+
+  // Handle when a single country is selected
+  const handleCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const countryCode = e.target.value;
+    setCountries((prevCountries) => {
+      const newCountriesMap = new Map(prevCountries);
+      const country = newCountriesMap.get(countryCode);
+      if (country) {
+        newCountriesMap.set(countryCode, {
+          ...country,
+          checked: !country.checked,
+        });
+      }
+      return newCountriesMap;
+    });
+  };
+
+  // Handle when all countries are selected
+  const handleSelectAll = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const isChecked = e.target.checked;
+    setCountries((prevCountries) => {
+      const newCountriesMap = new Map(prevCountries);
+      newCountriesMap.forEach((country) => {
+        country.checked = isChecked;
+      });
+      return newCountriesMap;
+    });
+  };
+
+  // Check if all countries are selected (for select all checkbox)
+  const isCheckedAll = [...countries.values()].every(
+    (country: CountryItem) => country.checked
+  );
 
   return (
     <>
-      <div>HOLA MUNDO</div>
+      <h1>Country List</h1>
+      <ul className="unstyled-list text-align-left">
+        <li>
+          <input
+            type="checkbox"
+            value="all"
+            name="all"
+            id="id-all"
+            checked={isCheckedAll}
+            onChange={handleSelectAll}
+          />
+          <label htmlFor="id-all">Select All</label>
+        </li>
+        {[...countries.values()].map((country) => (
+          <li key={country.countryCode}>
+            <input
+              type="checkbox"
+              value={country.countryCode}
+              name={country.name}
+              id={`id-${country.countryCode}`}
+              checked={country.checked}
+              onChange={handleCheck}
+            />
+            <label htmlFor={`id-${country.countryCode}`}>{country.name}</label>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
